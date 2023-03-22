37173095e60743c1df6d68f101b078218dce7116
Guillermo <guillermoml2002@gmail.com>
2023-03-22 11:04:07 +0100

    Arreglo del buscado en cabecera noLogin

--- scripts/navegacion.js
+++ scripts/navegacion.js
@@ -1,12 +1,20 @@
+class HeaderLogin extends HTMLElement{
+    constructor(){
+        super()
+    }
+    connectedCallback(){
+        this.innerHTML = '<head><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined"/><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></head> <nav><div class="left-elements"><a href="#">☰ Todas las categorias</a><form action="./" method="get"><div class="searchbar"><input type="text" class="searchbarinput" name="q" placeholder="Search"><button type="submit" class="searchbarbutton"><i class="material-icons">search</i></button></div></form></div><div class="center-element"><h1><a href="#"> GameMall</a></h1></div><div class="right-elements"><a href="#"><span class="material-icons-outlined icon">person</span></a><span id="UserName">UserName</span></div></nav>'
+    }
+}
+
+
 class HeaderNoLogin extends HTMLElement{
     constructor(){
         super()
     }
     connectedCallback(){
-        this.innerHTML = '<nav><div class="left-elements"><a href="#">☰ Todas las categorias</a><form action="./" method="get"><div class="searchbar"><input type="text" class="searchbarinput" name="q" placeholder="Search"><button type="submit" class="searchbarbutton"><i class="material-icons">search</i></button></div></form></div><div class="center-element"><h1><a href="#"> GameMall</a></h1></div><div class="right-elements"><a href="#"><span class="material-icons-outlined icon">person</span></a><span id="UserName">UserName</span></div></nav>'
-    }
+        this.innerHTML = '<nav id="cabecera_nologin"><div class="left-elements"><a href="#" id="slidebar" style="font-size: larger;">☰</a><form action="./" method="get"><div class="searchbar"><input type="text" class="searchbarinput" name="q" placeholder="Search"><button type="submit" class="searchbarbutton"><i class="material-icons">search</i></button></div></form></div><div class="center-element"><h1 class="MainNames"><a href="#"> GameMall</a></h1></div><div class="right-elements"><button>Login</button><button>Registrarse</button></div></nav>'
 }
-class HeaderLogin extends HTMLElement{
-
 }
 window.customElements.define('cabecera-nologin', HeaderNoLogin)
+window.customElements.define('cabecera-login', HeaderLogin)
