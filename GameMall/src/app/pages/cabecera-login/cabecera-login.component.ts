e340c2517bfb052bf96b892ab16227731911259f
OmarLopezGuillen <omar.estudio36@gmail.com>
2023-03-27 09:26:05 +0100

    Primeros componentes

---
+++ GameMall/src/app/pages/cabecera-login/cabecera-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera-login',
+  templateUrl: './cabecera-login.component.html',
+  styleUrls: ['./cabecera-login.component.css']
+})
+export class CabeceraLoginComponent {
+
+}
