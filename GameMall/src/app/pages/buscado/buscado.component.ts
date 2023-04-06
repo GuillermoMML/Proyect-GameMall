d4117a9250073dbf7e24b2365d4571f38c6b7f9d
GuillermoMML <guillermoml2002@gmail.com>
2023-04-06 12:49:43 +0000

    Sccript de buscado utilizando fetch

--- GameMall/src/app/pages/buscado/buscado.component.ts
+++ GameMall/src/app/pages/buscado/buscado.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-buscado',
   templateUrl: './buscado.component.html',
   styleUrls: ['./buscado.component.css']
 })
-export class BuscadoComponent {
+export class BuscadoComponent implements OnInit {
 
+  datosBuscados: HTMLElement = document.createElement('div');
+
+  ngOnInit(){
+    this.imagesJson().then(container => {
+      this.datosBuscados = container;
+    });
+  }
+
+  imagesJson(): Promise<HTMLElement> {
+    return fetch('../../../data/datos.json')
+        .then(response => response.json())
+        .then(data => {
+            const container: HTMLElement | null = document.getElementById('containerbuscado');
+            if (container) {
+                data.buscado.map((data: any) => {
+                    const div = document.createElement('div');
+
+                    const img = document.createElement('img');
+                    img.className = "buscadoimg";
+                    img.src = data.img;
+                    img.classList.add('buscadoimg');
+                    div.appendChild(img);
+
+                    const title = document.createElement('label');
+                    title.innerHTML = data.title;
+                    title.htmlFor = "Title";
+                    title.className = "title";
+                    title.classList.add('title');
+                    div.appendChild(title);
+
+                    const price = document.createElement('label');
+                    price.innerHTML = `${data.price}`;
+                    price.className = "price";
+                    price.classList.add('price');
+                    div.appendChild(price);
+
+                    container.appendChild(div);
+                });
+            }
+            return container as HTMLElement;
+        });
+  }
 }
