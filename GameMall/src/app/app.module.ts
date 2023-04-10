945ece1997d68af810f699753e177e3044c87b95
loukavanhoucke480@gmail.com <loukavanhoucke480@gmail.com>
2023-04-10 07:50:18 +0000

    register, login, mainpage

--- GameMall/src/app/app.module.ts
+++ GameMall/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PrincipalComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ GameMall/src/app/pages/principal/principal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import 'src/app/js/slider.js';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent implements OnInit{
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+}
