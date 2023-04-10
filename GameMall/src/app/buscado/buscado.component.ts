eab7033b00553da928fc9be136f2a2b46d4adaa0
Guillermo <guillermoml2002@gmail.com>
2023-04-10 10:38:42 +0200

    Data Binding Buscado

--- GameMall/src/app/buscado/buscado.component.ts
+++ GameMall/src/app/buscado/buscado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import datos from '../../assets/datos.json';
 
 
@@ -7,41 +7,9 @@ import datos from '../../assets/datos.json';
   templateUrl: './buscado.component.html',
   styleUrls: ['./buscado.component.css']
 })
+
 export class BuscadoComponent {
 
-  datosBuscados: any;
+  public buscado: { id: number; img: string; title: string; price: string; }[] =datos.buscado;
 
-  ngOnInit(){
-    this.datosBuscados = this.imagesJson();
-
-  }
-  imagesJson() {
-    const container: HTMLElement | null = document.getElementById('containerbuscado');
-    if (container) {
-      datos.buscado.forEach((data: any) => {
-        const div = document.createElement('div');
-
-        const img = document.createElement('img');
-        img.className = "buscadoimg";
-        img.src = data.img;
-        img.classList.add('buscadoimg');
-        div.appendChild(img);
-
-        const title = document.createElement('label');
-        title.innerHTML = data.title;
-        title.htmlFor = "Title";
-        title.className = "title";
-        title.classList.add('title');
-        div.appendChild(title);
-
-        const price = document.createElement('label');
-        price.innerHTML = `${data.price}`;
-        price.className = "price";
-        price.classList.add('price');
-        div.appendChild(price);
-
-        container.appendChild(div);
-      });
-    }
-  }
 }
