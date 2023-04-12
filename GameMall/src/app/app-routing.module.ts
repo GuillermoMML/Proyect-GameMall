425432b3872e79b26e8aab99be5605a261caff0b
loukavanhoucke480@gmail.com <loukavanhoucke480@gmail.com>
2023-04-12 10:41:51 +0000

    routing

--- GameMall/src/app/app-routing.module.ts
+++ GameMall/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+
+const routes: Routes = [
+  { path: '', component: PrincipalComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- GameMall/src/app/app.module.ts
+++ GameMall/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,6 +29,7 @@ import { Compra2Component } from './pages/compra2/compra2.component';
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [RouterModule],  
   providers: [],
   bootstrap: [AppComponent]
 })
