16db20889eda82222ac00e2877859b149663a59f
OmarLopezGuillen <omar.estudio36@gmail.com>
2023-04-13 16:42:00 +0100

    Actualizando pagina pago

--- GameMall/src/app/pages/compra1/compra1.component.ts
+++ GameMall/src/app/pages/compra1/compra1.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import datos from '../../../assets/json/datos.json'
 
 @Component({
   selector: 'app-compra1',
   templateUrl: './compra1.component.html',
   styleUrls: ['./compra1.component.css']
 })
-export class Compra1Component {
+export class Compra1Component{
 
+  recomendados: any;
+
+  ngOnInit(){
+    this.recomendados = this.mostrarRecomendados();
+  }
+
+  mostrarRecomendados() {
+    
+          interface Data {
+            id: number;
+            img: string;
+            title: string;
+            price: string;
+          }
+          const container: HTMLElement | null = document.getElementById('sugerencias');
+          if(container){
+            datos.juegos.forEach((data: Data) => {
+              const div = document.createElement('div');
+
+              const img = document.createElement('img');
+              img.className = "recomendado-img;"
+              img.src = data.img;
+              img.classList.add('recomendado-img')
+              div.appendChild(img)
+
+              const title = document.createElement('label')
+              title.innerHTML = data.title;
+              title.htmlFor = "Title";
+              title.className = "title";
+              title.classList.add('title')
+              div.appendChild(title);
+
+              const price = document.createElement('label');
+              price.innerHTML = `${data.price}`;
+              price.className = "price";
+              price.classList.add('price')
+              div.appendChild(price);
+
+               
+              container.appendChild(div)
+          });
+          
+          }
+          return container;
+}
 }
