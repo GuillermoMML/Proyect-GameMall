4cc492f5d01aaa6abd968d56c882bd149a7ddb76
OmarLopezGuillen <omar.estudio36@gmail.com>
2023-04-20 22:38:28 +0100

    Añadido bootstrap, fontawesome y subido /node_modules

---
+++ GameMall/node_modules/@angular/compiler-cli/bundles/private/migrations.js
@@ -0,0 +1,42 @@
+
+      import {createRequire as __cjsCompatRequire} from 'module';
+      const require = __cjsCompatRequire(import.meta.url);
+    
+import {
+  DynamicValue,
+  PartialEvaluator,
+  PotentialImportKind,
+  PotentialImportMode,
+  StaticInterpreter,
+  forwardRefResolver
+} from "../chunk-KXJ5LO4I.js";
+import {
+  TypeScriptReflectionHost,
+  reflectObjectLiteral
+} from "../chunk-UPBLAOWL.js";
+import "../chunk-4EAKVGWO.js";
+import {
+  Reference
+} from "../chunk-I7N7PLKQ.js";
+import "../chunk-K2U2VZ7S.js";
+import "../chunk-HK36RNP3.js";
+import "../chunk-SRFZMXHZ.js";
+export {
+  DynamicValue,
+  PartialEvaluator,
+  PotentialImportKind,
+  PotentialImportMode,
+  Reference,
+  StaticInterpreter,
+  TypeScriptReflectionHost,
+  forwardRefResolver,
+  reflectObjectLiteral
+};
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+//# sourceMappingURL=migrations.js.map
