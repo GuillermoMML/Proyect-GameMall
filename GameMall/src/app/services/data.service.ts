07d672a71139c21f5ba888d2064595970df0dc5e
GuillermoMML <guillermoml2002@gmail.com>
2023-04-29 21:54:05 +0100

    Sistema de buscado mediante firebase

--- GameMall/src/app/services/data.service.ts
+++ GameMall/src/app/services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionData } from "@angular/fire/firestore";
+import { Firestore, collection, collectionData, query, where } from "@angular/fire/firestore";
 import { Slider, Tendencias, TopVentas, Buscado } from '../interfaces/interfaces';
 import { Observable } from 'rxjs';
 
@@ -29,4 +29,25 @@ export class DataService {
     const buscadoRef = collection(this.firestore, 'buscado')
     return collectionData(buscadoRef, {idField: 'id'}) as Observable<Buscado[]>;
   }
+
+  getBuscadosCoincidentes(title: string): Observable<Buscado[]> {
+    const buscadoRef = collection(this.firestore, 'buscado');
+  
+    if (title === '') {
+      return collectionData(buscadoRef, {idField: 'id'}) as Observable<Buscado[]>;
+    } else {
+      // Convertir la cadena de búsqueda en una lista de palabras clave
+      const palabrasClave = title.split(' ');
+  
+      // Utilizar `array-contains` para buscar coincidencias en la lista de palabras clave
+      const coincidencias = where('title', 'array-contains-any', palabrasClave);
+      const queryBuscados = query(buscadoRef, coincidencias);
+  
+      return collectionData(queryBuscados, { idField: 'id' }) as Observable<Buscado[]>;
+    }
+  }
+  
+  
+  
+  
 }
