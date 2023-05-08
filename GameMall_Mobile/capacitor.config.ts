eb651858bc965cdf95191c64b383b14d9539cb16
loukavanhoucke480@gmail.com <loukavanhoucke480@gmail.com>
2023-05-08 08:18:14 +0000

    Ionic

---
+++ GameMall_Mobile/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GameMall_Mobile',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
