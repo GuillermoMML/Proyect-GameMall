b7acc9b742b556d0655943a62edc48287f3d5782
loukavanhoucke480@gmail.com <loukavanhoucke480@gmail.com>
2023-05-15 09:07:20 +0000

    page4

--- GameMall_Mobile/src/app/pages/master/master.page.ts
+++ GameMall_Mobile/src/app/pages/master/master.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-master',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterPage implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
 
+  elementos: Observable<any[]>;
+
+  constructor(private router: Router, private firestore: Firestore) {
+    this.elementos = this.getElementos();
+  }
+
+  getElementos(): Observable<any[]> {
+    const elementosRef = collection(this.firestore, 'tendencias'); // Reemplaza 'tu-coleccion-firebase' con el nombre de tu colección en Firebase
+    return collectionData(elementosRef) as Observable<any[]>;
+  }
+
+  verDetalle(elemento: any) {
+    // Redirigir a la página de detalle y pasar el ID del elemento como parámetro
+    this.router.navigate(['/detalle', elemento.id]);
+  }
 }
